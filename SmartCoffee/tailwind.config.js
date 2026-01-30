28939bdac1500cce03d9ecd012405e89974ff45c
QHuyyyyy <huy120904@gmail.com>
2026-01-30 13:14:13 +0700

    fixed nativewind config

--- SmartCoffee/tailwind.config.js
+++ SmartCoffee/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/*", "./app/(tabs)/*", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3E2723",
+        secondary: "#D7CCC8",
+        accent: "#8D6E63",
+        "background-light": "#FAFAFA",
+        "background-dark": "#121212",
+        "surface-light": "#FFFFFF",
+        "surface-dark": "#1E1E1E",
+        "text-light": "#2D2D2D",
+        "text-dark": "#E0E0E0",
+      },
+    },
   },
   plugins: [],
 }
