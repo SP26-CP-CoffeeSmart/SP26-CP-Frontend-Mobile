66471c868802815140a5c7b7d4a188322648534a
nguyendang03 <npmd2004@gmail.com>
2026-03-14 21:40:55 +0700

    create import note after import request

--- SmartCoffee/services/api.ts
+++ SmartCoffee/services/api.ts
@@ -109,6 +109,9 @@ export const API_ENDPOINTS = {
   supplier: {
     list: () => `${AUTH_BASE_URL}/Supplier`,
   },
+  importNote: {
+    create: () => `${AUTH_BASE_URL}/ImportNote`,
+  },
   shopInventory: {
     getById: (id: number) => `${AUTH_BASE_URL}/ShopInventory/${id}`,
     getByShop: (shopId: number) => `${AUTH_BASE_URL}/ShopInventory/by-shop/${shopId}`,
